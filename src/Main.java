f84ec85bb8a44eb136e1d041d4da912a17a8a8a4
Jhoner <jhonerquispe31@gmail.com>
2026-08-13 15:14:04 -0500

    cambio

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
-public class Main{
+public class Main {
 
-    public void meses(){
-        String[] meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
-        System.out.println("Los meses del año son: ");
-        for (String mes : meses) {
-            System.out.println(mes);
+    public static void meses(){
+        String[] mes={"Enero", "Febrero", "Marzo", "Abril", "Mayo",
+        "Junio", "Julio", "Agosto", "Setiembre",
+                "Octubre", "Noviembre", "Diciembre"};
+
+        System.out.println("Tamaño:"+mes.length);
+        for (String m:mes){
+            System.out.println(m);
+        }
+
+        mes[11]="";
+        System.out.println("");
+        for (String m:mes){
+            if(!m.equals(""))
+            System.out.println(m);
         }
     }
 
+    public static void imprimirMatriz(int [][] m){
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                if(m[i][j]!=0){
+                    System.out.print(m[i][j]+"\t");
+                }else{
+                    System.out.print(" \t");
+                }
+            }
+            System.out.println("");
+        }
+
+    }
+
+
     public static void main(String[] args) {
-        Main main = new Main();
-        main.meses();
+        meses();
+        int [][] m={{0,2}, {3,4}};
+        imprimirMatriz(m);
     }
 }
